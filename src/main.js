74ccc4e53150f431f7e00ee368bdfabdfb088219
Felipe <pife00@gmail.com>
2020-07-17 23:59:17 -0500

    Ya se ve por el puerto 3000

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ Vue.use(VueAxios, axios);
 Vue.use(VModal);
 Vue.component('downloadCsv', JsonCSV);
 
-axios.defaults.baseURL= "http://ec2-3-128-34-113.us-east-2.compute.amazonaws.com/api";
+axios.defaults.baseURL= "http://ec2-3-128-34-113.us-east-2.compute.amazonaws.com:3000/api";
 
 
 Vue.config.productionTip = false;
