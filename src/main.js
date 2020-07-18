e79a8f568c4377143d8a9cbdb5e7cb5ec26e2cee
Felipe <pife00@gmail.com>
2020-07-17 19:44:54 -0500

    Añadido las API rutas

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import store from './store/index'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-import VModal from 'vue-js-modal'
-import Chartkick from 'vue-chartkick'
-import Chart from 'chart.js'
-import JsonCSV from 'vue-json-csv'
-import excel from 'vue-excel-export'
-import VueGoogleCharts from 'vue-google-charts'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import store from './store/index';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import VModal from 'vue-js-modal';
+import Chartkick from 'vue-chartkick';
+import Chart from 'chart.js';
+import JsonCSV from 'vue-json-csv';
+import excel from 'vue-excel-export';
+import VueGoogleCharts from 'vue-google-charts';
 
 
-Vue.use(excel)
+Vue.use(excel);
 
-Vue.use(VueGoogleCharts)
-Vue.use(Chartkick)
-Vue.use(Chartkick.use(Chart))
+Vue.use(VueGoogleCharts);
+Vue.use(Chartkick);
+Vue.use(Chartkick.use(Chart));
 Vue.use(VueAxios, axios);
 Vue.use(VModal);
-Vue.component('downloadCsv', JsonCSV)
+Vue.component('downloadCsv', JsonCSV);
 
-axios.defaults.baseURL= "http://localhost:3000";
+axios.defaults.baseURL= "http://localhost:3000/api";
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
