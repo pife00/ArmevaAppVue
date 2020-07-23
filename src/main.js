964966452fa2ab230d4e827d4d4c6e4f67f97ff3
Felipe <pife00@gmail.com>
2020-07-22 22:25:58 -0500

    fix minor

--- src/main.js
+++ src/main.js
@@ -23,8 +23,8 @@ Vue.use(VueAxios, axios);
 Vue.use(VModal);
 Vue.component('downloadCsv', JsonCSV);
 
-//axios.defaults.baseURL= "http://ec2-3-128-34-113.us-east-2.compute.amazonaws.com:3000/api";
-axios.defaults.baseURL ="http://localhost:3000/api";
+axios.defaults.baseURL= "http://ec2-3-128-34-113.us-east-2.compute.amazonaws.com:3000/api";
+//axios.defaults.baseURL ="http://localhost:3000/api";
 Vue.config.productionTip = false;
 new Vue({
   router,
